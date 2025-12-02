89e8389f50269a8aad5d3405717b8ed2ec80e61e
Luana <luana.cam20@gmail.com>
2025-12-01 22:06:59 -0300

    chore: atualiza configurações do projeto
    - Adiciona polling para hot reload no next.config.js
    - Remove arquivo COLORS.md não utilizado
    - Atualiza configurações do Tailwind

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  // Garantir que o Fast Refresh funcione corretamente
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      }
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
